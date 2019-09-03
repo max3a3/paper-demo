76ad35195cdd544c543816b77914b8a635697ca9
max3a3 <isplitpoly@gmail.com>
2019-09-03 12:42:34 -0700

    1 file modified

--- component/DirectPaper.js
+++ component/DirectPaper.js
@@ -1,41 +1,52 @@
 import paper from 'paper'
-import React ,{useRef,useEffect} from 'react'
+import React, { useRef, useEffect } from 'react'
 
 function getdrawing() {
 
-        function drawLine(start_pt, stle) {
-            //Point data structure has methods for other operation, so it is good
-            //  to use instead of rewriting in react
-            //  how to store this in redux?
+  function drawLine(start_pt, stle) {
+    //Point data structure has methods for other operation, so it is good
+    //  to use instead of rewriting in react
+    //  how to store this in redux?
 
-            let start = new paper.Point(start_pt);
-            // Move to start and draw a line from there
-            let path = new paper.Path();
-            path.moveTo(start);
-            // Note that the plus operator on Point objects does not work
-            // in JavaScript. Instead, we need to call the add() function:
-            path.lineTo(start.add([200, 30]));
-            path.style = style
+    let start = new paper.Point(start_pt);
+    // Move to start and draw a line from there
+    let path = new paper.Path();
+    path.moveTo(start);
+    // Note that the plus operator on Point objects does not work
+    // in JavaScript. Instead, we need to call the add() function:
+    path.lineTo(start.add([200, 30]));
+    path.style = style
 
-        }
+  }
 
-        let style = {
-            fillColor: new paper.Color(1, 0, 0),
-            strokeColor: 'black',
-            strokeWidth: 2
-        };
-        drawLine([10, 5], style)
-        drawLine([70, 5], style)
+  let style = {
+    fillColor: new paper.Color(1, 0, 0),
+    strokeColor: 'black',
+    strokeWidth: 2
+  };
+  drawLine([10, 5], style)
+  drawLine([70, 5], style)
 }
 export function DirectPaper() {
-  let canvas_ref =useRef(null)
-    useEffect(() => {
+  let canvas_ref = useRef(null)
+  useEffect(() => {
 
-        paper.setup(canvas_ref.current);
-        getdrawing()
+    paper.setup(canvas_ref.current);
+    getdrawing()
 
-    })
-  return(   <canvas className='tool_canvas' ref={canvas_ref}>
-            </canvas>)
+  })
+  return (
+    <div className="flex_container">
+      <div className="flex_item">
+        layer1<br />
+        layer2<br />
+        <br />
+      </div>
+
+      <canvas className='tool_canvas flex_item' ref={canvas_ref}>
+      </canvas>
+
+      )
+</div>
+  )
 }
-         
