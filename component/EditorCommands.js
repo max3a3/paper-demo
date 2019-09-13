c6c4c91309ac7d8528dac7dc4d60ca5474e3690d
wilkin <wilkin4@gmail.com>
2019-09-12 18:13:20 -0700

    add initial_data loading

--- component/EditorCommands.js
+++ component/EditorCommands.js
@@ -1,31 +1,40 @@
-import React, {Fragment,useRef} from 'react';
+import React, {Fragment, useRef, useEffect} from 'react';
 import {Button} from 'reactstrap'
 import {useDispatch, useSelector} from "react-redux";
 import {loadCanvasData, newCanvas} from "../store/canvas/actions";
+import initial_data from "../data/initial_data.json"
+import paper from "paper";
 
 export function EditorCommands() {
-    let textAreaRef = useRef(null)
-    let canvas = useSelector(state => state.canvas)
-    const dispatch = useDispatch();
-    function onSave()  {
-        let canvas_json = JSON.stringify(canvas, undefined, 4)
-        textAreaRef.current.value = canvas_json
-    }
-    function onLoad() {
-        dispatch(loadCanvasData(JSON.parse(textAreaRef.current.value)))
-    }
-    function onNew() {
-        dispatch(newCanvas())
+  let textAreaRef = useRef(null)
+  let canvas = useSelector(state => state.canvas)
+  const dispatch = useDispatch();
+  useEffect(() => {
+    textAreaRef.current.value = JSON.stringify(initial_data, undefined, 4)
+    dispatch(loadCanvasData(initial_data))
+  },[])
 
-    }
-    return (
-        <Fragment>
-            layer1<br/>
-            <Button onClick={onNew}>new</Button>
-            <Button onClick={onSave}>save</Button>
-            <Button onClick={onLoad}>load</Button>
-            <textarea className="save_area" ref={textAreaRef}></textarea>
-        </Fragment>
-    )
+  function onSave() {
+    let canvas_json = JSON.stringify(canvas, undefined, 4)
+    textAreaRef.current.value = canvas_json
+  }
+
+  function onLoad() {
+    dispatch(loadCanvasData(JSON.parse(textAreaRef.current.value)))
+  }
+
+  function onNew() {
+    dispatch(newCanvas())
+  }
+
+  return (
+      <Fragment>
+        layer1<br/>
+        <Button onClick={onNew}>new</Button>
+        <Button onClick={onSave}>save</Button>
+        <Button onClick={onLoad}>load</Button>
+        <textarea className="save_area" ref={textAreaRef}></textarea>
+      </Fragment>
+  )
 
 }
