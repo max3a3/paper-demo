980eebbe2a7f3cb80db9d4c61de5f373624183df
wilkin <wilkin4@gmail.com>
2020-05-09 22:04:05 -0700

    useDrop for dropper

--- treeview/InsertTarget.js
+++ treeview/InsertTarget.js
@@ -1,24 +1,11 @@
 import React from "react";
 import {
-  DropTarget,
+  DropTarget, useDrop
 } from "react-dnd";
 
 
-import { TYPE, } from "./DraggedNode";
-import { Styles } from "./InsertTarget.styles";
-
-
-
-
-
-
-
-
-
-
-
-
-
+import {TYPE,} from "./DraggedNode";
+import {Styles} from "./InsertTarget.styles";
 
 
 const TreeViewInsertTarget = (props) =>
@@ -33,7 +20,8 @@ const TreeViewInsertTarget = (props) =>
               )
             }
         >
-          <div style={ props.isDropping ? Styles.insertTargetMarkerDropping : {} } />
+          x
+          <div style={props.isDropping ? Styles.insertTargetMarkerDropping : {}}/>
         </div>
     );
 
@@ -73,19 +61,62 @@ const handleDrop = (
         })
 );
 
+// todo test that we do have nested? [moved in useDrop drop: func()] i thought it is all linear?
 const nodeTarget = {
   drop: (props, monitor, component) => monitor.didDrop()
-      ? undefined // some child already handled drop
-      : handleDrop(props, monitor, component, monitor.getItem() ),
-  canDrop: (props, monitor) => handleCanDrop(props, monitor, monitor.getItem() ),
+      ? undefined // we have nested drop target, this indicate some child already handled drop
+      : handleDrop(props, monitor, component, monitor.getItem()),
+  canDrop: (props, monitor) => handleCanDrop(props, monitor, monitor.getItem()),
 };
 
 const collectNodeDropProps =
     (connect, monitor) => ({
       connectDropTarget: connect.dropTarget(),
       canDrop: monitor.canDrop(),
-      isDropping: monitor.isOver({ shallow: true }) && monitor.canDrop(),
+      isDropping: monitor.isOver({shallow: true}) && monitor.canDrop(),
     });
 
-export const DroppableTreeViewInsertTarget =
+export const DroppableTreeViewInsertTargetX =
     DropTarget([TYPE], nodeTarget, collectNodeDropProps)(TreeViewInsertTarget);
+
+export function DroppableTreeViewInsertTarget(props) {
+  // first param is what is returned from the collect
+  const [{canDrop, isDropping}, drop] = useDrop({
+    accept: TYPE,
+    drop: (item, monitor) => monitor.didDrop()
+        ? undefined // we have nested drop target, this indicate some child already handled drop
+        : handleDrop(props, monitor, null, item),
+    canDrop: (item, monitor) => (
+        !(
+            props.parentNode === item.parentNode &&
+            (
+                props.parentChildIndex === item.parentChildIndex ||
+                props.parentChildIndex === item.parentChildIndex + 1
+            )
+        ) &&
+        !item.allSourceIDs.contains(props.parentNode ? props.parentNode.id : null)
+    ),
+
+
+    collect: (monitor) => ({
+      canDrop: monitor.canDrop(),
+      isDropping: monitor.isOver({shallow: true}) && monitor.canDrop(),
+    })
+
+
+  })
+  return (<div
+          ref={drop}
+          style={
+            Object.assign(
+                {},
+                props.insertBefore ? Styles.insertBeforeTarget : Styles.insertAfterTarget,
+                canDrop ? Styles.insertTargetCanDrop : {},
+                isDropping ? Styles.insertTargetDropping : {}
+            )
+          }
+      >
+        <div style={isDropping ? Styles.insertTargetMarkerDropping : {}}/>
+      </div>
+  )
+}
